7850e8491d6a145cd98c1925c028af8ff720e410
ant30 <ant30tx@gmail.com>
2013-04-23 13:46:05 +0200

    Split mail validator in two steps, verified and wrong mails. Put emails in files

--- eduid_signup/tests/test_validators.py
+++ eduid_signup/tests/test_validators.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-
+from os import path
 
 import unittest
 
@@ -8,44 +7,23 @@ from eduid_signup.validators import email_format_validator
 
 class EmailValidatorTests(unittest.TestCase):
 
-    def test_emailvalidator(self):
-        verified_emails = [
-            "user@example.com",
-            "user@example.co.uk",
-            "user@subdomain.example.co.uk",
-            "user.name@example.com",
-            "user+filter@example.com",
-            "user.name+filter@example.co.uk",
-            "averylongerlongerlongerlongerlongeremailaddress@example.com",
-            "user@example.com",
-            "user6@example.com",
-            "66user@example.com",
-            "user-test@example.com",
-            "user_test@example.com",
-            "user@example-test.com",
-            "User@example.com",
-            "USER6@example.com",
-            "#user@example.com",
-            "?user@example.com",
-            "us#er@more.domains.example.com",
-            "user@localhost.localdomain",
-            "root@localhost.localdomain",
-        ]
+    def read_emails(self, filename):
+        basepath = path.abspath(path.dirname(__file__))
+        emails = []
+        with open(path.join(basepath, filename), "r") as emailsfile:
+            emaillines = emailsfile.readlines()
+            for emailline in emaillines:
+                emails.append(emailline.strip())
+        return emails
+
+    def test_verified_emails(self):
+        verified_emails = self.read_emails("verified_emails.txt")
 
         for email in verified_emails:
             self.assertEqual(None, email_format_validator(email))
 
-        wrong_emails = [
-            "#user@exañple.com",
-            "usé@more.domains.example.com",
-            "userâ@more.domains.example.com",
-            "user'more@.domains.example.com",
-            "us·er@more.domains.example.com",
-            "a@com",
-            "com",
-            "userlocalhost",
-            "user@localhost",
-        ]
+    def test_wrong_emails(self):
+        wrong_emails = self.read_emails("wrong_emails.txt")
 
         for email in wrong_emails:
             self.assertEqual({
