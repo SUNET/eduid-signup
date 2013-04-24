8ca790aca714b3927a77a25a367e7bca5ef812a3
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2013-04-24 12:17:59 +0200

    Include pyramid_sna and remove other inclusions in the development.ini since they are already included in the includeme function of the application

--- eduid_signup/__init__.py
+++ eduid_signup/__init__.py
@@ -81,6 +81,7 @@ def main(global_config, **settings):
         config.include('pyramid_mailer')
 
     config.include('pyramid_tm')
+    config.include('pyramid_sna')
 
     # global directives
     config.add_static_view('static', 'static', cache_max_age=3600)
