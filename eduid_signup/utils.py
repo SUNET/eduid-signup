364d95ff121c3284ce7d78b3a31a935cfc804137
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2013-04-26 08:53:26 +0200

    Make it compatible with Python 3, which does not have an explicit unicode function

--- eduid_signup/utils.py
+++ eduid_signup/utils.py
@@ -2,9 +2,11 @@ from uuid import uuid4
 
 from pyramid.httpexceptions import HTTPInternalServerError
 
+from eduid_signup.compat import text_type
+
 
 def generate_verification_link(request):
-    code = unicode(uuid4())
+    code = text_type(uuid4())
     link = request.route_url("email_verification_link", code=code)
     return (link, code)
 
