3378bce3cc730236734cce9998257f798731adc7
Fredrik Thulin <fredrik@thulin.net>
2013-06-04 08:26:51 +0200

    Try to fix _id retreival on e-mail confirmation.

--- eduid_signup/emails.py
+++ eduid_signup/emails.py
@@ -40,16 +40,19 @@ def send_verification_mail(request, email):
 
     mailer.send(message)
 
-    user_id = request.db.registered.update({
-        "email": email,
-    }, {
-        '$set': {
-            "email": email,
-            "date": datetime.utcnow(),
-            "code": code,
-            "verified": False,
-        }
-    }, upsert=True, safe=True)
+    docu = {'email': email,
+            }
+    request.db.registered.update(docu,
+                                 {
+            '$set': {
+                "email": email,
+                "date": datetime.utcnow(),
+                "code": code,
+                "verified": False,
+                }
+            }, upsert=True, safe=True, manipulate=True)
+
+    user_id = str(docu.get('_id')) # manipulate=True populates docu['_id'] above
 
     # Send the signal to the attribute manager so it can update
     # this user's attributes in the IdP
