2d503280ad8492c465d9ae0fbc13d750d11c336f
Fredrik Thulin <fredrik@thulin.net>
2013-10-16 16:39:55 +0200

    fix another {x}, argh.

--- eduid_signup/views.py
+++ eduid_signup/views.py
@@ -105,10 +105,10 @@ def success(request):
     if 'email' not in request.session:
         home_url = request.route_url("home")
         return HTTPFound(location=home_url)
-        
+
     email = request.session['email']
     secret = request.registry.settings.get('auth_shared_secret')
-    timestamp = "{x}".format(int(time.time()))
+    timestamp = "{:x}".format(int(time.time()))
     nonce = os.urandom(16).encode('hex')
     auth_token = generate_auth_token(secret, email, nonce, timestamp)
 
