e42c2aa34e81a909fba5ce9fcd65420b36d85ffd
Fredrik Thulin <fredrik@thulin.net>
2014-03-04 13:44:53 +0100

    Semantic versioning, now at 0.2.0.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-version = '0.1'
+version = '0.2.0'
 
 requires = [
     'eduid_am==0.3.0',
