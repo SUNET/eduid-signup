a76c148a2678f660924ab2ebdb03a95508624913
Stefan Wold <swold@sunet.se>
2014-03-25 08:59:06 +0100

    Version 0.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-version = '0.2.1'
+version = '0.2.2'
 
 requires = [
     'eduid_am==0.4.2',
