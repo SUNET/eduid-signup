1784c9abc4887c26a3c41bcad9a2d7178f3cf587
Enrique Pérez <eperez@yaco.es>
2014-04-08 11:40:56 +0200

    prevent weird locale names (en:English sv:Svenska), EDUID-167

--- eduid_signup/i18n.py
+++ eduid_signup/i18n.py
@@ -13,7 +13,11 @@ def locale_negotiator(request):
     if cookie_lang and cookie_lang in available_languages:
         return cookie_lang
 
-    return request.accept_language.best_match(available_languages)
+    locale_name = request.accept_language.best_match(available_languages)
+
+    if locale_name not in available_languages:
+        locale_name = settings.get('default_locale_name', 'sv')
+    return locale_name
 
 
 def add_localizer(event):
