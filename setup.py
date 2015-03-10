1afd67f64d949d5b9135e7c96fa4abfa2b93166d
Fredrik Thulin <fredrik@thulin.net>
2015-03-10 12:00:06 +0100

    loosen all requirements

--- setup.py
+++ setup.py
@@ -12,21 +12,22 @@ version = '0.4.2-dev'
 
 requires = [
     'eduid_am>=0.5.1',
-    'wsgi_ratelimit==0.1',
-    'vccs_client==0.4.1',
-    'pymongo==2.6.3',
-    'pyramid==1.4.1',
-    'pyramid_beaker==0.7',
-    'pyramid_debugtoolbar==1.0.4',
-    'pyramid_jinja2==1.6',
-    'pyramid_mailer==0.11',
-    'pyramid_tm==0.7',
-    'pyramid_sna==0.3.1',
-    'waitress==0.8.2',
-    'recaptcha-client==1.0.6',
-    'pwgen==0.4',
-    'proquint==0.1.0',
-    'gunicorn==18.0',
+    'vccs_client>=0.4.1',
+
+    'wsgi_ratelimit>=0.1',
+    'pymongo>=2.6.3',
+    'pyramid>=1.4.1',
+    'pyramid_beaker>=0.7',
+    'pyramid_debugtoolbar>=1.0.4',
+    'pyramid_jinja2>=1.6',
+    'pyramid_mailer>=0.11',
+    'pyramid_tm>=0.7',
+    'pyramid_sna>=0.3.1',
+    'waitress>=0.8.2',
+    'recaptcha-client>=1.0.6',
+    'pwgen>=0.4',
+    'proquint>=0.1.0',
+    'gunicorn>=18.0',
 ]
 
 if sys.version_info[0] < 3:
