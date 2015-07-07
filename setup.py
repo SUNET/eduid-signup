a74de2186ea202b2b37a64c94c615f912e7a8355
John Van de Meulebrouck Brendgard <john@nordu.net>
2015-07-07 13:35:03 +0200

    Changed eduid-am requirement to > eduid_am>=0.5.3,<0.5.9

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 version = '0.4.4'
 
 requires = [
-    'eduid_am==0.5.3',
+    'eduid_am>=0.5.3,<0.5.9',
     'vccs_client>=0.4.1',
     'wsgi_ratelimit>=0.1',
     'pymongo>=2.8,<3',
