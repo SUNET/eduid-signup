9beb9c59675a269cf6578aebe188649c533d3c6d
Fredrik Thulin <fredrik@thulin.net>
2016-03-02 18:56:51 +0100

    move Dockerfile here

---
+++ docker/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Install all requirements
+#
+
+set -e
+set -x
+
+#apt-get -y install \
+
+PYPI="https://pypi.nordu.net/simple/"
+ping -c 1 -q pypiserver.docker && PYPI="http://pypiserver.docker:8080/simple/"
+
+echo "#############################################################"
+echo "$0: Using PyPi URL ${PYPI}"
+echo "#############################################################"
+
+/opt/eduid/bin/pip install       -i ${PYPI} raven
+
+/opt/eduid/bin/pip freeze
