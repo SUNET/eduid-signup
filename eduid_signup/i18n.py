ba332d9eaa42fe01fbc40c138ae91e1bb0843d84
John Van de Meulebrouck Brendgard <john@sunet.se>
2017-09-29 17:09:56 +0200

    Added debug logging for change of language

--- eduid_signup/i18n.py
+++ eduid_signup/i18n.py
@@ -1,5 +1,8 @@
 from pyramid.i18n import TranslationStringFactory, get_localizer
 
+import logging
+logger = logging.getLogger(__name__)
+
 translation_domain = 'eduid_signup'
 TranslationString = TranslationStringFactory(translation_domain)
 
@@ -9,14 +12,24 @@ def locale_negotiator(request):
     available_languages = settings['available_languages'].keys()
     cookie_name = settings['lang_cookie_name']
 
+    logger.debug("locale_negotiator, available_languages: {}".format(available_languages))
+    logger.debug("locale_negotiator, cookie_name: {}".format(cookie_name))
+
     cookie_lang = request.cookies.get(cookie_name, None)
+
+    logger.debug("locale_negotiator, cookie_lang: {}".format(cookie_lang))
+
     if cookie_lang and cookie_lang in available_languages:
+        logger.debug("locale_negotiator, returning cookie_lang: {}".format(cookie_lang))
         return cookie_lang
 
     locale_name = request.accept_language.best_match(available_languages)
 
     if locale_name not in available_languages:
         locale_name = settings.get('default_locale_name', 'sv')
+
+    logger.debug("locale_negotiator, locale_name: {}".format(locale_name))
+
     return locale_name
 
 
